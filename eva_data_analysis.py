f80a4a524e11ee7a96558c2db18bd81143963451
Steve Crouch <s.crouch@software.ac.uk>
2026-05-06 14:27:35 +0100

    Add additional analysis functions

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
+import re
 
 def main(input_file, output_file, graph_file):
     print("--START--")
 
     # Read the data from JSON file
     eva_data = read_json_to_dataframe(input_file)
+
+    # Calculate and add crew size to data
+    eva_data = add_crew_size_column(eva_data) # added this line
 
     # Convert and export data to CSV file
     write_dataframe_to_csv(eva_data, output_file)
@@ -84,7 +88,7 @@ def text_to_duration(duration):
         duration_hours (float): The duration in hours
     """
     hours, minutes = duration.split(":")
-    duration_hours = int(hours) + int(minutes)/6  # there is an intentional bug on this line (should divide by 60 not 6)
+    duration_hours = int(hours) + int(minutes)/60  # there is an intentional bug on this line (should divide by 60 not 6)
     return duration_hours
 
 
@@ -120,3 +124,34 @@ if __name__ == "__main__":
     graph_file = 'results/cumulative_eva_graph.png'
     main(input_file, output_file, graph_file)
 
+def calculate_crew_size(crew):
+    """
+    Calculate the size of the crew for a single crew entry
+
+    Args:
+        crew (str): The text entry in the crew column containing a list of crew member names
+
+    Returns:
+        (int): The crew size
+    """
+    if crew.split() == []:
+        return None
+    else:
+        return len(re.split(r';', crew))-1
+
+def add_crew_size_column(df):
+    """
+    Add crew_size column to the dataset containing the value of the crew size
+
+    Args:
+        df (pd.DataFrame): The input data frame.
+
+    Returns:
+        df_copy (pd.DataFrame): A copy of the dataframe df with the new crew_size variable added
+    """
+    print('Adding crew size variable (crew_size) to dataset')
+    df_copy = df.copy()
+    df_copy["crew_size"] = df_copy["crew"].apply(
+        calculate_crew_size
+    )
+    return df_copy
